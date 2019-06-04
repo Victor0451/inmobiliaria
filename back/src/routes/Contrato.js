ae41b7a059896bc8b143b0d75817310cc4d94800
victor0451 <victor.m.longo@gmail.com>
2019-06-04 20:22:52 -0300

    Se termino el contrato con el autocompletado, y se agrego el contradocumento tmb con el autocompletado. Tambien se agrego las funciones de impresion discriminada por el id del contenedor

--- back/src/routes/Contrato.js
+++ back/src/routes/Contrato.js
@@ -2,37 +2,36 @@ const express = require("express");
 const router = express.Router();
 const mysqlConnection = require("../db/database");
 
-//GET BY ID 
+//GET BY ID
 
 router.get("/getcontratoselect/:id", (req, res, next) => {
-    mysqlConnection.query(
-      "SELECT * FROM contrato WHERE id_contrato = ?",
-      [req.params.id],
-      (err, rows, fields) => {
-        if (!err) {
-          res.json(rows[0]);
-        } else {
-          console.log(err);
-        }
+  mysqlConnection.query(
+    "SELECT * FROM contrato WHERE id_contrato = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows[0]);
+      } else {
+        console.log(err);
       }
-    );
-  });
-
+    }
+  );
+});
 
 //INSERT
 
 router.post("/postcontrato", (req, res) => {
-  
-
   const data = {
     id_contrato: req.body.id_contrato,
     dni_locatario: req.body.dni_locatario,
     dni_locatario2: req.body.dni_locatario2,
     uf_tiponum: req.body.uf_tiponum,
-    locador: req.body.locador
+    locador: req.body.locador,
+    pagos_contrato: req.body.pagos_contrato,
+    pagos_pagare: req.body.pagos_pagare
   };
 
-  console.log(data)
+  console.log(data);
 
   const sql = "INSERT INTO contrato SET ?";
   mysqlConnection.query(sql, data, (err, result) => {
