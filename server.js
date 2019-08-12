d38249d3fedde24588c9df8985ec36302bbe2407
victor0451 <victor.m.longo@gmail.com>
2019-08-12 11:36:17 -0300

    instalacion de corss env para discriminacion de arranque con propiedades de produccion y desarrollo

--- server.js
+++ server.js
@@ -20,17 +20,21 @@ app.use(require('./src/routes/Locatario'));
 app.use(require('./src/routes/Locador'));
 app.use(require('./src/routes/Contrato'));
 
-process.env.NODE_ENV = '';
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+
+    console.log(process.env.NODE_ENV)
+
     // Set static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+} else {
+    console.log(process.env.NODE_ENV)
+};
 
 
 // server listening
